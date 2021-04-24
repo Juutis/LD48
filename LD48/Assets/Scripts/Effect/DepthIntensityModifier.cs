48fb55d5b033236fbb1e66150dfd9df0f440a8d6
Juutis <jutkula@gmail.com>
2021-04-24 11:50:16 +0300

    Added depth darkening

---
+++ LD48/Assets/Scripts/Effect/DepthIntensityModifier.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class DepthIntensityModifier : MonoBehaviour
+{
+    public float maxDepth = 10.0f;
+
+    private Light2D light;
+    private Vector2 startPosition;
+    private float startIntensity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        light = GetComponent<Light2D>();
+        startIntensity = light.intensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var t = (startPosition.y - transform.position.y) / maxDepth;
+        var intensity = Mathf.Lerp(startIntensity, 0, t);
+        light.intensity = intensity;
+    }
+}
