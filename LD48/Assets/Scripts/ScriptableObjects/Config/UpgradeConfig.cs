0ff946a0c951739a2ef31155523571d6089e2693
Matias Pitkänen <M2tias@GMail.com>
2021-04-24 18:53:27 +0300

    UI assets, upgrade config, moved scriptable objects script files

---
+++ LD48/Assets/Scripts/ScriptableObjects/Config/UpgradeConfig.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UpgradeConfig", menuName = "Configs/New UpgradeConfig")]
+public class UpgradeConfig : ScriptableObject
+{
+    public List<Upgrade> damage;
+    public List<Upgrade> attackSpeed;
+    public List<Upgrade> movementSpeed;
+    public List<Upgrade> lights;
+    public List<Upgrade> health;
+}
+
+[Serializable]
+public struct Upgrade
+{
+    public string loreText;
+    public string description;
+    public float value;
+    public float price;
+}
