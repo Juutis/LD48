fcd03f171948a8fd271fd0c1bf5e4325b06ec299
Juutis <jutkula@gmail.com>
2021-04-25 09:47:08 +0300

    Tweaked TORPEDO

--- LD48/Assets/Scripts/Character/Torpedo.cs
+++ LD48/Assets/Scripts/Character/Torpedo.cs
@@ -11,40 +11,49 @@ public class Torpedo : MonoBehaviour
     [SerializeField]
     private float followTime = 5f;
     [SerializeField]
-    private float createdTime;
+    private float turnSpeed = 180;
+    [SerializeField]
+    private float damage = 1;
+
+    [SerializeField]
+    private ParticleSystem particles;
 
     private Rigidbody2D rigidBody;
+    private Collider2D collider;
+    private Renderer renderer;
     private Vector3 followTarget;
 
-    private float damage = 1;
+    private float createdTime;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        renderer = GetComponent<Renderer>();
         createdTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rigidBody.velocity = transform.right * speed;
         var mousePosition = Input.mousePosition;
         followTarget = (Vector2)Camera.main.ScreenToWorldPoint(mousePosition);
-    }
-
-    private void FixedUpdate()
-    {
 
         if (Time.time - createdTime <= followTime)
         {
             float angleDiff = Vector2.SignedAngle(followTarget - transform.position, transform.right);
-            transform.Rotate(Vector3.back, Mathf.Clamp(angleDiff, -5, 5));
+            transform.Rotate(Vector3.back, Mathf.Clamp(angleDiff, -turnSpeed * Time.deltaTime, turnSpeed * Time.deltaTime) );
         }
         else if (Time.time - createdTime > lifeTime)
         {
             Destroy(gameObject);
         }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.velocity = transform.right * speed;
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
@@ -54,6 +63,19 @@ public class Torpedo : MonoBehaviour
         {
             hurtable.Hurt(damage);
         }
+        Kill();
+    }
+
+    public void Kill()
+    {
+        particles.Stop();
+        collider.enabled = false;
+        renderer.enabled = false;
+        Invoke("ReallyKill", 1.5f);
+    }
+
+    public void ReallyKill()
+    {
         Destroy(gameObject);
     }
 }
