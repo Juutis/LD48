d50b5204eb907bf169c710204f0c64c2ef574240
Juutis <jutkula@gmail.com>
2021-04-26 07:42:36 +0300

    Added destroyable stalagmites

---
+++ LD48/Assets/Scripts/Item/Stalagmite.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Hurtable))]
+public class Stalagmite : MonoBehaviour
+{
+    [SerializeField]
+    private HealthConfig healthConfig;
+
+    private Hurtable hurtable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hurtable = GetComponent<Hurtable>();
+        hurtable.Initialize(healthConfig);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Damaged(float amount)
+    {
+        if (amount > 0)
+        {
+            hurtable.Hurt(-amount);
+        }
+        if (amount > 11.0f)
+        {
+            Kill();
+        }
+    }
+
+    public void Kill()
+    {
+        Destroy(gameObject);
+    }
+}
