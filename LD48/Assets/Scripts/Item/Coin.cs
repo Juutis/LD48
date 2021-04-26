eda4c148e6e59816f2877fc08bef011b640b3db2
Juutis <jutkula@gmail.com>
2021-04-26 09:18:08 +0300

    Fix pickups

--- LD48/Assets/Scripts/Item/Coin.cs
+++ LD48/Assets/Scripts/Item/Coin.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(Random.Range(-1.0f, 1.0f), 1.0f), ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
